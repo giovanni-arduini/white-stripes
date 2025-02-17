fa59cac28e465aecb96238c92a25b100216acb3b
giovanni-arduini <gionnibigudd@gmail.com>
2025-02-17 11:09:29 +0100

    ADD integrate error handling middleware for 404 and general errors

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound");
 app.use(express.static("public"))
+
 
 app.get("/", (req, res)=>{
     res.send("Server is running")
 })
+
+app.use(notFound)
+
+app.use(errorsHandler)
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
