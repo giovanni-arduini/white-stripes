ebdc9ae93004d197eccc65d28e1ea102857910b6
giovanni-arduini <gionnibigudd@gmail.com>
2025-02-17 16:45:38 +0100

    FIX showUser user.spots

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,28 @@
 const connection = require("../data/db")
 
 function showUser(req, res) {
-    // recupera un utente e le sue segnalazioni di parchegghi
+    // recupera un utente e le sue segnalazioni di parcheggi
     const userId = parseInt(req.params.id);
-    let sql = `SELECT * FROM utenti WHERE id = ${userId}`;
+    let sql = `SELECT * FROM utenti WHERE id = ?`;
 
-    connection.query(sql, [userId], (err, user) => {
+    connection.query(sql, [userId], (err, results) => {
         if (err) return res.status(500).json({ message: err.message });
-        if (user.length === 0) return res.status(404).json({ message: 'Utente non trovato' });
-        res.json(user[0]);
+        if (results.length === 0) return res.status(404).json({ message: 'Utente non trovato' });
+
+        const user = results[0];
+        console.log(user);
+
+        sql = `SELECT * FROM parcheggi WHERE id_utente = ? ORDER BY data_creazione DESC`;
+        connection.query(sql, [userId], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message });
+            user.spots = results;
+
+            res.json(user);
+            console.log(user);
+        });
     });
 }
+
 
 
 module.exports = {showUser}
